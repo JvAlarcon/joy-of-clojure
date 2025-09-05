f3e7495ccb553f9f3e75a880df20f219167f9875
João <joao.alarcon25@gmail.com>
2025-09-05 19:28:16 -0300

    Implementation of exercises from chapter 2; Reading of chapter 5

--- src/joy_of_clojure/chapter_five.clj
+++ src/joy_of_clojure/chapter_five.clj
@@ -1,2 +1,92 @@
 (ns joy-of-clojure.chapter-five)
 
+(def ds (into-array [:willie :barnabas :adam]))
+(seq ds)
+(aset ds 1 :quentin)
+(seq ds)
+
+(def ds [:willie :barnabas :adam])
+ds
+(def ds1 (replace {:barnabas :quentin} ds))
+ds
+ds1
+
+(= [1 2 3] '(1 2 3)) ; => true
+(= [1 2 3] #{1 2 3}) ; => false
+
+(vec (range 10))
+
+(let [my-vector [:a  :b :c]]
+  (into my-vector (range 10)))
+
+(into (vector-of :int) [Math/PI 2 1.3])
+
+(into (vector-of :char) [100 101 102 245])
+
+(into (vector-of :int) [1 2 623879184509187538351714814091290])
+
+(def a-to-j (vec (map char (range 65 75))))
+a-to-j
+
+(nth a-to-j 4)
+(get a-to-j 4)
+(a-to-j 4)
+
+(seq a-to-j)
+(rseq a-to-j)
+
+(assoc a-to-j 3 "No longer D")
+(replace {2 :a, 4 :b} [1 2 3 2 3 4 1 3 4])
+
+(def matrix
+  [[1 2 3]
+   [4 5 6]
+   [7 8 9]])
+(get-in matrix [1 2])
+(get-in matrix [0 2])
+(get-in matrix [2 0])
+
+(assoc-in matrix [1 2] 'x)
+
+(update-in matrix [1 2] * 100)
+
+(defn neighbors
+  ([size yx] (neighbors [[-1 0] [1 0] [0 -1] [0 1]]
+                        size
+                        yx))
+  ([deltas size yx]
+   (filter (fn [new-yx]
+             (every? #(< -1 % size) new-yx))
+           (map #(vec (map + yx %))
+                deltas))))
+
+(neighbors 3 [1 1])
+
+(def my-stack [1 2 3])
+(peek my-stack)
+(pop my-stack)
+(conj my-stack 4)
+(+ (peek my-stack) (peek (pop my-stack)))
+
+(subvec a-to-j 3 6)
+
+(first {:width 10, :height 20, :depth 15})
+(vector? (first {:width 10, :height 20, :depth 15}))
+(doseq [[dimension amount] {:width 10, :height 20, :depth 15}]
+  (println (str (name dimension) ":") amount "inches"))
+
+
+(defmethod print-method clojure.lang.PersistentQueue
+  [q, w]
+  (print-method '<- w)
+  (print-method (seq q) w)
+  (print-method '-< w))
+clojure.lang.PersistentQueue/EMPTY
+
+(def schedule
+  (conj clojure.lang.PersistentQueue/EMPTY
+        :wake-up :shower :brush-teeth))
+schedule
+(peek schedule)
+(pop schedule)
+(rest schedule) ; don't use that for queue
--- src/joy_of_clojure/chapter_two.clj
+++ src/joy_of_clojure/chapter_two.clj
@@ -217,3 +217,55 @@ java.util.Locale/JAPAN
 ;(throw-catch #(/ 10 "a"))
 (throw-catch #(throw (Exception. "Crybaby")))
 
+(defn xors [max-x max-y]
+  (for [x (range max-x) y (range max-y)]
+    [x y (bit-xor x y)]))
+
+(for [meth (.getMethods java.awt.Frame)
+  :let [name (.getName meth)]
+  :when (re-find #"Vis" name)]
+  name)
+
+(def frame (java.awt.Frame.))
+(.isVisible frame)
+(.setVisible frame true)
+(.setSize frame (java.awt.Dimension. 200 200))
+
+(def gfx (.getGraphics frame))
+(.fillRect gfx 100 100 50 75)
+(.setColor gfx (java.awt.Color. 255 128 0))
+(.fillRect gfx 100 150 75 50)
+
+(defn xors [max-x max-y]
+  (for [x (range max-x) y (range max-y)]
+    [x y (bit-xor x y)]))
+
+(doseq [[x y xor] (xors 200 200)]
+  (.setColor gfx (java.awt.Color. xor xor xor))
+  (.fillRect gfx x y 1 1))
+
+(defn xors [xs ys]
+  (for [x (range xs) y (range ys)]
+    [x y (rem (bit-xor x y) 256)]))
+
+(defn clear [g] (.clearRect g 0 0 200 200))
+
+(defn f-values [f xs ys]
+  (for [x (range xs) y (range ys)]
+    [x y (rem (f x y) 256)]))
+
+(defn draw-values [f xs ys]
+  (clear gfx)
+  (.setSize frame (java.awt.Dimension. xs ys))
+  (doseq [[x y v] (f-values f xs ys)]
+    (.setColor gfx (java.awt.Color. v v v))
+    (.fillRect gfx x y 1 1)))
+
+(draw-values bit-and 256 256)
+(draw-values bit-and 500 500)
+(draw-values + 256 256)
+(draw-values * 500 500)
+(draw-values - 256 256)
+(draw-values bit-or 256 256)
+(draw-values bit-xor 256 256)
+(draw-values = 256 256)
